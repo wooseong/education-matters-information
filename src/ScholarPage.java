acc37defee6b091f89c3294801a401dc5d4f8725
Bang <forresti3979@gmail.com>
2017-12-09 20:53:30 -0800

    장학 페이지 완성

--- src/ScholarPage.java
+++ src/ScholarPage.java
@@ -3,7 +3,6 @@ import javax.swing.JPanel;
 
 public class ScholarPage extends JPanel {
 	private JLabel name;
-	private JLabel total;
 	private SelfCreateTable table;
 	private DBController db;
 	
@@ -11,14 +10,24 @@ public class ScholarPage extends JPanel {
 		this.setLayout(null);
 		
 		this.name = new JLabel();
+		this.name.setBounds(10, 10, 200, 20);
 		this.add(name);
 		
-		this.total = new JLabel();
-		this.add(total);
+		this.table = new SelfCreateTable();
+		this.table.setBounds(10, 40, 1010, 600);
+		this.add(table);
+		
 	}
 	
 	public void initPage() {
-		this.name.setText(User.USERNAME);
+		this.name.setText(User.USERNAME + "님 장학 내역");
 		
+		String[] colum = {"지급일", "내용", "금액 (원)"};
+		String query = "select `when`,`content`,`money` from scholar where id='" + User.LOGINID + "'";
+		this.table.makeTable(query);
+		this.table.getTable().getColumnModel().getColumn(0).setHeaderValue(colum[0]);
+		this.table.getTable().getColumnModel().getColumn(1).setHeaderValue(colum[1]);
+		this.table.getTable().getColumnModel().getColumn(2).setHeaderValue(colum[2]);
+		this.table.getTable().repaint();
 	}
 }
